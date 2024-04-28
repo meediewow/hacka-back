d2b8591fc603b5346f438b80a398422f68053ad9
mididevich <mididevich@gmail.com>
2024-04-28 11:01:46 +0300

    fix: swagger module

--- src/modules/user/dto/sitters.dto.ts
+++ src/modules/user/dto/sitters.dto.ts
@@ -1,14 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsNumber,
-  IsOptional,
-  IsString,
-  ValidateNested
-} from 'class-validator';
+import { IsNumber, IsOptional, ValidateNested } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 import { PeriodDto } from '../../../network/dto/period.dto';
 import { SorterDto } from '../../../network/dto/sorter.dto';
+
+import { UserDto } from './user.dto';
 
 export class SittersRequestDto {
   @IsNumber(undefined, { each: true })
@@ -32,52 +29,7 @@ export class SittersRequestDto {
   sorter?: SorterDto;
 }
 
-export class SitterDto {
-  @IsString()
-  @ApiProperty({ type: 'string', example: 'xxxxxxxxxxxxxxxxxxx' })
-  _id: string;
-
-  @IsString()
-  @ApiProperty({ type: 'string', example: 'Homer Simpson' })
-  name: string;
-
-  @IsNumber()
-  @ApiProperty({ type: 'number', example: 10 })
-  rating: number;
-
-  @IsNumber()
-  @IsOptional()
-  @ApiProperty({ type: 'number', example: 73 })
-  price?: number;
-
-  @IsString()
-  @ApiProperty({ type: 'string', example: 'https://btc.ru' })
-  photo: string;
-
-  @IsString()
-  @ApiProperty({ type: 'string', example: 'Limassol, Cyprus' })
-  address: string;
-
-  @IsNumber()
-  @ApiProperty({ type: 'number', example: 10 })
-  countOrders: number;
-
-  constructor(data: SitterDto) {
-    this._id = data._id;
-    this.name = data.name;
-    this.rating = data.rating;
-    this.price = data.price;
-    this.photo = data.photo;
-    this.address = data.address;
-    this.countOrders = data.countOrders;
-  }
-
-  static fromEntity(entity: SitterDto) {
-    return new SitterDto(entity);
-  }
-}
-
 export class SittersResponseDto {
-  @ApiProperty({ type: SitterDto, isArray: true })
-  list: SitterDto[];
+  @ApiProperty({ type: UserDto, isArray: true })
+  list: UserDto[];
 }
